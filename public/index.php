574ee4ea801c143e386d99e88ee1ff6fbe7220e8
calemb <calemb@gmail.com>
2018-03-23 11:06:48 +0100

    Remove example route, move route from index to routes.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ if (PHP_SAPI == 'cli-server') {
 }
 //
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/classes/RepoData.php';
+
 
 session_start();
 
@@ -19,45 +19,6 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 
 $app = new \Slim\App($settings);
-function SanitizeInput($input)
-{
-    $sanitizedInput = trim(strip_tags($input));
-    return $sanitizedInput;
-}
-
-//route to proper app path
-$app->get('/api/{user1}/{repo1}/{user2}/{repo2}', function ($request, $response, $args) {
-    
-    //validate input data!
-    $user1 = SanitizeInput($args['user1']);
-    $repo1 = SanitizeInput($args['repo1']);
-
-    $user2 = SanitizeInput($args['user2']);
-    $repo2 = SanitizeInput($args['repo2']);
-
-    //gather data about repos
-    $r1 = new \STD\GitHubStatistics\RepoData($user1, $repo1);
-    $firstRepo = $r1->GetRepoData();
-
-    //to prevent API request again for the same (limited API calls per hour without enterprise!)
-    if ($user1 === $user2 && $repo1 === $repo2) {
-        $secRepo = $firstRepo;
-    } else {
-        $r2 = new \STD\GitHubStatistics\RepoData($user2, $repo2);
-        $secRepo = $r2->GetRepoData();
-    }
-
-
-    $comparision = \STD\GitHubStatistics\RepoData::Compare($firstRepo, $secRepo);
-
-    //create response data array
-    $data = array('0' => $firstRepo, '1' => $secRepo, '2' => $comparision);
-
-    //response with json
-    $newResponse = $response->withJson($data);
-
-    return $newResponse;
-});
 
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,43 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-
+require __DIR__ . '/../src/classes/RepoData.php';
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+function SanitizeInput($input)
+{
+    $sanitizedInput = trim(strip_tags($input));
+    return $sanitizedInput;
+}
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//route to proper app path
+$app->get('/api/{user1}/{repo1}/{user2}/{repo2}', function ($request, $response, $args) {
+
+    //validate input data!
+    $user1 = SanitizeInput($args['user1']);
+    $repo1 = SanitizeInput($args['repo1']);
+
+    $user2 = SanitizeInput($args['user2']);
+    $repo2 = SanitizeInput($args['repo2']);
+
+    //gather data about repos
+    $r1 = new \STD\GitHubStatistics\RepoData($user1, $repo1);
+    $firstRepo = $r1->GetRepoData();
+
+    //to prevent API request again for the same (limited API calls per hour without enterprise!)
+    if ($user1 === $user2 && $repo1 === $repo2) {
+        $secRepo = $firstRepo;
+    } else {
+        $r2 = new \STD\GitHubStatistics\RepoData($user2, $repo2);
+        $secRepo = $r2->GetRepoData();
+    }
+
+    $comparision = \STD\GitHubStatistics\RepoData::Compare($firstRepo, $secRepo);
+
+    //create response data array
+    $data = array('0' => $firstRepo, '1' => $secRepo, '2' => $comparision);
+
+    //response with json
+    $newResponse = $response->withJson($data);
+
+    return $newResponse;
 });
